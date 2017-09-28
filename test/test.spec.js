76b7adcc9c10452852d5dce6e640c3c044dd7693
Rumkin <dev@rumk.in>
2017-09-29 02:38:34 +0300

    [src,test] #1 start version 2, add new emojis; update test

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,8 +5,8 @@ const emojis = require('..');
 describe('emoji-img', function() {
     it('Should return map of images', function() {
         assert.ok(emojis instanceof Map, 'It\'s a Map');
-        assert.equal(emojis.size, 882, 'Images count is 882');
-        assert.ok(fs.existsSync(emojis.get('smiley')), 'Smiley exists on fs');
-        assert.ok(fs.existsSync(emojis.get('thumbsup')), 'Thumbs up exists on fs');
+        assert.equal(emojis.size, 2373, 'Images count is 2373');
+        assert.ok(fs.existsSync(emojis.get('helicopter')), 'Helicopter exists on fs');
+        assert.ok(fs.existsSync(emojis.get('bacon')), 'Bacon up exists on fs');
     });
 });
